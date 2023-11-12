2ea35cc98a2933c6c252def14965e590952b09c7
Khaled Obaid <Khalid.AlShikhSaleh@cubesplatform.com>
2023-11-12 13:22:17 +0300

    + Add spenner GIF and uploade App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,41 @@
 <template>
-  <div class="column-container">
-    <div class="table-wrapper">
-      <div class="table-scroll">
-        <table>
-          <th>{{ singleColumn[0] }}</th>
-          <tr v-for="(key, val, idx) in singleColumn[1]" :key="val">
-            <td>{{ key[singleColumn[0]] }}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
+  <div>
+    <Dataviewer
+      v-for="(itm, idx) in dataObject"
+      :key="itm"
+      :requestURL="itm.url"
+      @resolvedData="columnCreator(idx, $event)"
+    >
+      <ColumnViewer
+        v-for="key in itm.dataKeys"
+        :key="key"
+        :singleColumn="[key, itm.dataObject]"
+      />
+    </Dataviewer>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue"
-defineProps(["singleColumn"])
+import { ref } from "vue"
+import Dataviewer from "./assets/components/data-viewer/data-viewer.vue"
+import ColumnViewer from "./assets/components/data-viewer/column-viewer.vue"
+const dataObject = ref([
+  {
+    url: "https://654b92025b38a59f28ef5698.mockapi.io/data",
+    dataKeys: [],
+    dataObject: [],
+  },
+  {
+    url: "https://run.mocky.io/v3/512a3387-8171-4578-a4ef-b01a7a3b28c4",
+    dataKeys: [],
+    dataObject: [],
+  },
+])
+const columnCreator = (indx, $event) => {
+  console.log($event[0])
+  dataObject.value[indx].dataObject = $event[0]
+  dataObject.value[indx].dataKeys = $event[1]
+}
 </script>
-<style scoped>
-table {
-  text-align: center;
-}
-table th {
-  color: azure;
-  background-color: #5a5656;
-  width: 200px;
-  float: left;
-}
-.column-container {
-  float: left;
-  margin-top: 10px;
-}
-table td {
-  border: 1px solid black;
-}
 
-.table-wrapper {
-  position: relative;
-}
-.table-scroll {
-  height: 200px;
-  overflow: auto;
-  margin-top: 20px;
-  border: 1px solid black;
-}
-.table-wrapper table {
-  width: 100%;
-}
-</style>
+<style scoped></style>
